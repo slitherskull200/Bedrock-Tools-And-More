1c54f325c372f026ce06117f8501f349b086092b
slitherskull200 <slitherskull200@gmail.com>
2021-06-26 18:31:23 +0100

    begun the addition of the bedrock armor set

---
+++ src/main/java/net/mcreator/randommod/item/BedrockArmorItem.java
@@ -0,0 +1,105 @@
+
+package net.mcreator.randommod.item;
+
+import net.minecraftforge.registries.ObjectHolder;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.api.distmarker.Dist;
+
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.item.crafting.Ingredient;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.Item;
+import net.minecraft.item.IArmorMaterial;
+import net.minecraft.item.ArmorItem;
+import net.minecraft.inventory.EquipmentSlotType;
+import net.minecraft.entity.Entity;
+import net.minecraft.block.Blocks;
+
+import net.mcreator.randommod.RandommodModElements;
+
+@RandommodModElements.ModElement.Tag
+public class BedrockArmorItem extends RandommodModElements.ModElement {
+	@ObjectHolder("randommod:bedrock_armor_helmet")
+	public static final Item helmet = null;
+	@ObjectHolder("randommod:bedrock_armor_chestplate")
+	public static final Item body = null;
+	@ObjectHolder("randommod:bedrock_armor_leggings")
+	public static final Item legs = null;
+	@ObjectHolder("randommod:bedrock_armor_boots")
+	public static final Item boots = null;
+	public BedrockArmorItem(RandommodModElements instance) {
+		super(instance, 23);
+	}
+
+	@Override
+	public void initElements() {
+		IArmorMaterial armormaterial = new IArmorMaterial() {
+			@Override
+			public int getDurability(EquipmentSlotType slot) {
+				return new int[]{13, 15, 16, 11}[slot.getIndex()] * 25;
+			}
+
+			@Override
+			public int getDamageReductionAmount(EquipmentSlotType slot) {
+				return new int[]{11, 13, 14, 12}[slot.getIndex()];
+			}
+
+			@Override
+			public int getEnchantability() {
+				return 47;
+			}
+
+			@Override
+			public net.minecraft.util.SoundEvent getSoundEvent() {
+				return (net.minecraft.util.SoundEvent) ForgeRegistries.SOUND_EVENTS.getValue(new ResourceLocation("block.anvil.use"));
+			}
+
+			@Override
+			public Ingredient getRepairMaterial() {
+				return Ingredient.fromStacks(new ItemStack(Blocks.BEDROCK, (int) (1)));
+			}
+
+			@OnlyIn(Dist.CLIENT)
+			@Override
+			public String getName() {
+				return "bedrock_armor";
+			}
+
+			@Override
+			public float getToughness() {
+				return 4f;
+			}
+
+			@Override
+			public float getKnockbackResistance() {
+				return 4f;
+			}
+		};
+		elements.items.add(() -> new ArmorItem(armormaterial, EquipmentSlotType.HEAD, new Item.Properties().group(ItemGroup.COMBAT)) {
+			@Override
+			public String getArmorTexture(ItemStack stack, Entity entity, EquipmentSlotType slot, String type) {
+				return "randommod:textures/models/armor/bedrockarmortexture_layer_" + (slot == EquipmentSlotType.LEGS ? "2" : "1") + ".png";
+			}
+		}.setRegistryName("bedrock_armor_helmet"));
+		elements.items.add(() -> new ArmorItem(armormaterial, EquipmentSlotType.CHEST, new Item.Properties().group(ItemGroup.COMBAT)) {
+			@Override
+			public String getArmorTexture(ItemStack stack, Entity entity, EquipmentSlotType slot, String type) {
+				return "randommod:textures/models/armor/bedrockarmortexture_layer_" + (slot == EquipmentSlotType.LEGS ? "2" : "1") + ".png";
+			}
+		}.setRegistryName("bedrock_armor_chestplate"));
+		elements.items.add(() -> new ArmorItem(armormaterial, EquipmentSlotType.LEGS, new Item.Properties().group(ItemGroup.COMBAT)) {
+			@Override
+			public String getArmorTexture(ItemStack stack, Entity entity, EquipmentSlotType slot, String type) {
+				return "randommod:textures/models/armor/bedrockarmortexture_layer_" + (slot == EquipmentSlotType.LEGS ? "2" : "1") + ".png";
+			}
+		}.setRegistryName("bedrock_armor_leggings"));
+		elements.items.add(() -> new ArmorItem(armormaterial, EquipmentSlotType.FEET, new Item.Properties().group(ItemGroup.COMBAT)) {
+			@Override
+			public String getArmorTexture(ItemStack stack, Entity entity, EquipmentSlotType slot, String type) {
+				return "randommod:textures/models/armor/bedrockarmortexture_layer_" + (slot == EquipmentSlotType.LEGS ? "2" : "1") + ".png";
+			}
+		}.setRegistryName("bedrock_armor_boots"));
+	}
+}
