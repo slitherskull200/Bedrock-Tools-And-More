96173e87d167d2f9f2c3c1b0cdf5ca9bcf247a75
slitherskull200 <slitherskull200@gmail.com>
2021-06-27 08:30:42 +0100

    Upped Generation of Compressed Cobble

--- src/main/java/net/mcreator/randommod/block/CompressedCobblestoneBlock.java
+++ src/main/java/net/mcreator/randommod/block/CompressedCobblestoneBlock.java
@@ -28,12 +28,15 @@ import net.minecraft.util.registry.Registry;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.RegistryKey;
+import net.minecraft.particles.ParticleTypes;
 import net.minecraft.loot.LootContext;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Item;
 import net.minecraft.item.BlockItem;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.client.renderer.RenderTypeLookup;
 import net.minecraft.client.renderer.RenderType;
+import net.minecraft.client.Minecraft;
 import net.minecraft.block.material.PushReaction;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.SoundType;
@@ -94,6 +97,27 @@ public class CompressedCobblestoneBlock extends RandommodModElements.ModElement 
 				return dropsOriginal;
 			return Collections.singletonList(new ItemStack(this, 1));
 		}
+
+		@OnlyIn(Dist.CLIENT)
+		@Override
+		public void animateTick(BlockState state, World world, BlockPos pos, Random random) {
+			super.animateTick(state, world, pos, random);
+			PlayerEntity entity = Minecraft.getInstance().player;
+			int x = pos.getX();
+			int y = pos.getY();
+			int z = pos.getZ();
+			if (true)
+				for (int l = 0; l < 20; ++l) {
+					double d0 = (x + random.nextFloat());
+					double d1 = (y + random.nextFloat());
+					double d2 = (z + random.nextFloat());
+					int i1 = random.nextInt(2) * 2 - 1;
+					double d3 = (random.nextFloat() - 0.5D) * 1.9D;
+					double d4 = (random.nextFloat() - 0.5D) * 1.9D;
+					double d5 = (random.nextFloat() - 0.5D) * 1.9D;
+					world.addParticle(ParticleTypes.CRIT, d0, d1, d2, d3, d4, d5);
+				}
+		}
 	}
 	private static Feature<OreFeatureConfig> feature = null;
 	private static ConfiguredFeature<?, ?> configuredFeature = null;
@@ -128,8 +152,8 @@ public class CompressedCobblestoneBlock extends RandommodModElements.ModElement 
 					return super.generate(world, generator, rand, pos, config);
 				}
 			};
-			configuredFeature = feature.withConfiguration(new OreFeatureConfig(CustomRuleTest.INSTANCE, block.getDefaultState(), 16)).range(10)
-					.square().func_242731_b(12);
+			configuredFeature = feature.withConfiguration(new OreFeatureConfig(CustomRuleTest.INSTANCE, block.getDefaultState(), 26)).range(30)
+					.square().func_242731_b(4);
 			event.getRegistry().register(feature.setRegistryName("compressed_cobblestone"));
 			Registry.register(WorldGenRegistries.CONFIGURED_FEATURE, new ResourceLocation("randommod:compressed_cobblestone"), configuredFeature);
 		}
